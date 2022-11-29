3fe79db7fb19937234c8b4afca6898f9066d0838
BraedenCu <braedencullen120@gmail.com>
2022-11-29 09:43:12 -0600

    working on local bluetooth connection

--- localtests/localBluetoothTest.py
+++ localtests/localBluetoothTest.py
@@ -1,5 +1,6 @@
 import bluetooth
 from subprocess import call
+from sh import bluetoothctl
 
 def connectTarget():
     target_name = "Bose QC35 II"
@@ -7,15 +8,19 @@ def connectTarget():
     nearby_devices = bluetooth.discover_devices()
     
     for bdaddr in nearby_devices:
-        if target_name == bluetooth.lookup_name( bdaddr ):
-            target_address = bdaddr
+        name = bluetooth.lookup_name( bdaddr )
+        print(bdaddr)
+        print(name)
+        if(name == target_name):
+            target_addr = bdaddr
             break
+
     if target_addr is not None:
         #target found
-        return True
+        return target_addr
     else:
         #target not found
-        return False
+        return None
 
 #home/dev/music/output.avi
 def playSound(fileAddress):
@@ -23,9 +28,10 @@ def playSound(fileAddress):
     basecmd = ["mplayer", "-ao", "alsa:device=bluetooth"]
     call(basecmd + [fileAddress])
 
-
 if __name__ == "__main__":
-    if connectTarget():
-        playSound()
+    addr = connectTarget()
+    if addr != None:
+        print(addr)
+        bluetoothctl("connect", addr)
     else:
-        print("unable to connect too a blue tooth device")
+        print("unable to connect too a bluetooth device")
