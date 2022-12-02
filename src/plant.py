eca563157312068cacfde86db32150145ab4414f
BraedenCu <braedencullen120@gmail.com>
2022-12-02 09:16:15 -0600

    conjoined sensor + bluetooth

--- src/plant.py
+++ src/plant.py
@@ -1,5 +1,10 @@
 import bluetooth
 from subprocess import call
+from sh import bluetoothctl
+import time
+import board
+from adafruit_seesaw.seesaw import Seesaw
+
 
 def connectTarget():
     target_name = "Bose QC35 II"
@@ -7,25 +12,52 @@ def connectTarget():
     nearby_devices = bluetooth.discover_devices()
     
     for bdaddr in nearby_devices:
-        if target_name == bluetooth.lookup_name( bdaddr ):
-            target_address = bdaddr
+        name = bluetooth.lookup_name( bdaddr )
+        print(bdaddr)
+        print(name)
+        if(name == target_name):
+            target_addr = bdaddr
             break
+
     if target_addr is not None:
         #target found
-        return True
+        print("Target Found: " + target_addr)
+        return target_addr
     else:
         #target not found
-        return False
+        return None
 
 #home/dev/music/output.avi
-def playSound(fileAddress):
-    fileAddress = "home/dev/music/output.avi"
+def playSound(files):
+    fileAddress = files[0]
     basecmd = ["mplayer", "-ao", "alsa:device=bluetooth"]
     call(basecmd + [fileAddress])
 
+def readPlantData():
+    # read moisture level through capacitive touch pad
+    touch = ss.moisture_read()
+    # read temperature from the temperature sensor
+    temp = ss.get_temp()
+    return touch
+
+def main(audio, moistureThreshhold):
+    addr = connectTarget()
+    audioFiles = audio
+
+    if addr != None:
+        bluetoothctl("connect", addr)
+    else:
+        print("unable to connect too a bluetooth device")
+
+    while(True):
+        moistureLevel = readPlantData()
+        if (moistureLevel < moistureThreshhold):
+            playSound(audioFiles[0])
+
 
 if __name__ == "__main__":
-    if connectTarget():
-        playSound()
-    else:
-        print("unable to connect too a blue tooth device")
+    i2c_bus = board.I2C()
+    ss = Seesaw(i2c_bus, addr=0x36)
+    moistureThreshhold = 0.5
+    fileAddressArr = ["home/dev/music/output.avi"]
+    main(fileAddressArr, moistureThreshhold)
